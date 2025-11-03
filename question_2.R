e8730f4de0d0be89b32fdd7a8505c507a7de1fd2
melielewis <37447564+melielewis@users.noreply.github.com>
2025-11-03 12:27:51 -0500

    Update question_2.R

--- question_2.R
+++ question_2.R
@@ -1,9 +1,8 @@
 if(!require("pacman")){install.packages("pacman")}
-pacman::p_load(tidyverse, quanteda, spacyr, tidymodels, 
-               textrecipes, udpipe, finetune, httr, rvest)
-
+pacman::p_load(tidyverse, spacyr, tidymodels, 
+               textrecipes, tidytext, finetune)
+# note: all models and results already saved in RData file
 load("~/bball.RData")
-
 cf_perf = cf_perf %>%
   mutate(games = as.integer(games),
          mp = as.numeric(mp),
@@ -31,39 +30,48 @@ cf_text = cf_text %>%
   mutate(pick_dif = pick - cf_pick,
          ws_dif = ws_48 - cf_ws48,
          vorp_dif = vorp - cf_vorp)
-cf_sum = cbind(cf_text %>% summarize(pick = round(mean(pick),2), 
-                                   games = round(mean(games, na.rm = T),2), 
-                                   per = round(mean(PER, na.rm = T),2), 
-                                   bpm = round(mean(bpm, na.rm = T),2), 
-                                   ws_48 = round(mean(ws_48, na.rm = T),2), 
-                                   vorp = round(mean(vorp, na.rm = T),2)) %>%
-  gather() %>%
-  rename(Substitute = value),
-  cf_text %>% summarize(pick = round(mean(cf_pick),2), 
-                        games = round(mean(cf_games, na.rm = T),2), 
-                        per = round(mean(cf_per, na.rm = T),2), 
-                        bpm = round(mean(cf_bpm, na.rm = T),2), 
-                        ws_48 = round(mean(cf_ws48, na.rm = T),2), 
-                        vorp = round(mean(cf_vorp, na.rm = T),2)) %>%
-    gather() %>%
-    rename(Protected = value) %>%
-    select(-key)) %>%
+
+# data for paired t-test (n = 74)
+cf_data2 = cf_text %>% 
+  arrange(cf_pick) %>%
+  distinct(idPlayer, .keep_all = T) %>%
+  arrange(pick) %>% distinct(cf_ProtectionId, .keep_all = TRUE) %>%
+  filter(!is.na(vorp) & !is.na(cf_vorp))
+# table results for paired t-test
+cf_sum = cbind(cf_data2 %>% summarize(pick = round(mean(pick),2), 
+                                      games = round(mean(games, na.rm = T),2),
+                                      per = round(mean(PER, na.rm = T),2), 
+                                      bpm = round(mean(bpm, na.rm = T),2),
+                                      ws_48 = round(mean(ws_48, na.rm = T),2),
+                                      vorp = round(mean(vorp, na.rm = T),2)) %>%
+                 gather() %>%
+                 rename(Substitute = value),
+               cf_data2 %>% summarize(pick = round(mean(cf_pick),2), 
+                                      games = round(mean(cf_games, na.rm = T),2), 
+                                      per = round(mean(cf_per, na.rm = T),2), 
+                                      bpm = round(mean(cf_bpm, na.rm = T),2),
+                                      ws_48 = round(mean(cf_ws48, na.rm = T),2),
+                                      vorp = round(mean(cf_vorp, na.rm = T),2)) %>%
+                 gather() %>%
+                 rename(Protected = value) %>%
+                 select(-key)) %>%
   mutate(Difference = round(Substitute - Protected,2),
          `T-Value` = NA,
          `P-Value` = NA)
-cf_sum$`T-Value`[1] = round(t.test(cf_text$pick, cf_text$cf_pick, paired = TRUE)$statistic,2)
-cf_sum$`T-Value`[2] = round(t.test(cf_text$games, cf_text$cf_games, paired = TRUE)$statistic,2)
-cf_sum$`T-Value`[3] = round(t.test(cf_text$PER, cf_text$cf_per, paired = TRUE)$statistic,2)
-cf_sum$`T-Value`[4] = round(t.test(cf_text$bpm, cf_text$cf_bpm, paired = TRUE)$statistic,2)
-cf_sum$`T-Value`[5] = round(t.test(cf_text$ws_48, cf_text$cf_ws48, paired = TRUE)$statistic,2)
-cf_sum$`T-Value`[6] = round(t.test(cf_text$vorp, cf_text$cf_vorp, paired = TRUE)$statistic,2)
-cf_sum$`P-Value`[1] = round(t.test(cf_text$pick, cf_text$cf_pick, paired = TRUE)$p.value,3)
-cf_sum$`P-Value`[2] = round(t.test(cf_text$games, cf_text$cf_games, paired = TRUE)$p.value,3)
-cf_sum$`P-Value`[3] = round(t.test(cf_text$PER, cf_text$cf_per, paired = TRUE)$p.value,3)
-cf_sum$`P-Value`[4] = round(t.test(cf_text$bpm, cf_text$cf_bpm, paired = TRUE)$p.value,3)
-cf_sum$`P-Value`[5] = round(t.test(cf_text$ws_48, cf_text$cf_ws48, paired = TRUE)$p.value,3)
-cf_sum$`P-Value`[6] = round(t.test(cf_text$vorp, cf_text$cf_vorp, paired = TRUE)$p.value,3)
+cf_sum$`T-Value`[1] = round(t.test(cf_data2$pick, cf_data2$cf_pick, paired = TRUE)$statistic,2)
+cf_sum$`T-Value`[2] = round(t.test(cf_data2$games, cf_data2$cf_games, paired = TRUE)$statistic,2)
+cf_sum$`T-Value`[3] = round(t.test(cf_data2$PER, cf_data2$cf_per, paired = TRUE)$statistic,2)
+cf_sum$`T-Value`[4] = round(t.test(cf_data2$bpm, cf_data2$cf_bpm, paired = TRUE)$statistic,2)
+cf_sum$`T-Value`[5] = round(t.test(cf_data2$ws_48, cf_data2$cf_ws48, paired = TRUE)$statistic,2)
+cf_sum$`T-Value`[6] = round(t.test(cf_data2$vorp, cf_data2$cf_vorp, paired = TRUE)$statistic,2)
+cf_sum$`P-Value`[1] = round(t.test(cf_data2$pick, cf_data2$cf_pick, paired = TRUE)$p.value,3)
+cf_sum$`P-Value`[2] = round(t.test(cf_data2$games, cf_data2$cf_games, paired = TRUE)$p.value,3)
+cf_sum$`P-Value`[3] = round(t.test(cf_data2$PER, cf_data2$cf_per, paired = TRUE)$p.value,3)
+cf_sum$`P-Value`[4] = round(t.test(cf_data2$bpm, cf_data2$cf_bpm, paired = TRUE)$p.value,3)
+cf_sum$`P-Value`[5] = round(t.test(cf_data2$ws_48, cf_data2$cf_ws48, paired = TRUE)$p.value,3)
+cf_sum$`P-Value`[6] = round(t.test(cf_data2$vorp, cf_data2$cf_vorp, paired = TRUE)$p.value,3)
 
+## svr data n = 110 with non-missing VORP
 set.seed(123)
 cf_split = initial_split(cf_text %>% filter(!is.na(vorp_dif)),
                          strata = vorp_dif)
@@ -96,44 +104,13 @@ cf_words = workflow() %>%
 tune_cf = tune_bayes(cf_words,
                      resamples = cf_folds,
                      iter = 20,
-                     metrics = metric_set(rmse,rsq, mae, mape),
+                     metrics = metric_set(rmse,rsq, mae),
                      control = control_bayes(save_pred = TRUE, 
                                              verbose_iter = TRUE))
 final_cf = finalize_workflow(cf_words,
                              tune_cf %>% select_best())                     
 cf_fit = last_fit(final_cf, cf_split, 
-                  metrics = metric_set(rmse,rsq, mae, mape))
+                  metrics = metric_set(rmse,rsq, mae))
 cf_fit %>% collect_metrics()                     
 cf_last_fit = extract_workflow(cf_fit)
-# cf_last_fit %>%
-#   tidy() %>%
-#   filter(term != "Bias") %>%
-#   mutate(
-#     sign = case_when(estimate > 0 ~ "Favors substitute pick ",
-#                      TRUE ~ "Favors protected pick"),
-#     estimate = abs(estimate),
-#     term = str_remove_all(term, "tfidf_desc2_")
-#   ) %>%
-#   group_by(sign) %>%
-#   top_n(20, estimate) %>%
-#   ungroup() %>%
-#   ggplot(aes(x = estimate,
-#              y = fct_reorder(term, estimate),
-#              fill = sign)) +
-#   geom_col(show.legend = FALSE) +
-#   scale_x_continuous(expand = c(0, 0)) +
-#   facet_wrap(~sign, scales = "free")
 
-tidy(cf_last_fit) %>%
-  filter(term != "Bias") %>%
-  slice_max(abs(estimate), n = 20) %>%
-  mutate(
-    term = str_remove_all(term, "tfidf_desc2_"),
-    term = fct_reorder(term, abs(estimate))
-  ) %>%
-  ggplot(aes(x = abs(estimate), y = term, fill = estimate > 0)) +
-  geom_col() +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_fill_discrete(labels = c("Favors substitute pick", "Favors protected pick")) +
-  labs(x = "Estimate from linear SVM (absolute value)", y = NULL, 
-       fill = "Substitute - Protected VORP?")
