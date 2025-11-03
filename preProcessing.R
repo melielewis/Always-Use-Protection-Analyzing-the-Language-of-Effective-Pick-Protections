3d0664e3e2dd85493adfd87da7c673002e4e8d45
melielewis <37447564+melielewis@users.noreply.github.com>
2025-11-03 11:57:42 -0500

    Update and rename tradeAnalysis.R to preProcessing.R

--- preProcessing.R
+++ preProcessing.R
@@ -1,31 +1,16 @@
-library(tidyverse)
-library(quanteda)
-library(quanteda.textstats)
-library(quanteda.textplots)
-library(spacyr)
-library(lattice)
-library(tidytext)
-library(reticulate)
-library(htmltools)
 if(!require("pacman")){install.packages("pacman")}
-pacman::p_load(tidyverse, quanteda, spacyr, tidymodels, 
-               textrecipes, udpipe, finetune, httr, rvest)
+pacman::p_load(tidyverse, quanteda, spacyr, httr, rvest)
+##### NOTE: code here is included for transparency, but all pre-processing steps/output are already saved in the RData file
+load("~/bball.RData") 
 
-load("~/bball.RData")
-
+## import from google sheets
 counterfactuals = googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Z-iRJuP4fk0pHZL93Vt18vnvd9X8kBzlLX52BZDE2r0/edit?gid=382933428#gid=382933428")
 cf = counterfactuals %>%
-  filter(!is.na(tradeId) & draftYear > 2004) %>%
+  filter(!is.na(tradeId) & draftYear > 2004) %>% #remove trades pre-2005 draft
   mutate(Protected = ifelse(is.na(Protected),0,Protected),
          Preference = ifelse(Preference == TRUE, 1, 0),
-       #  protectionApplied = ifelse(protectionApplied == TRUE, 1, 0),
-       #  preferenceApplied = ifelse(preferenceApplied == TRUE, 1, 0),
          n_prot_alts = ifelse(is.na(cf_ProtectionId), 0,
                          1+str_count(cf_ProtectionId, ",")),
-         n_pref_alts = ifelse(is.na(cf_PreferenceId), 0,
-                              1+str_count(cf_PreferenceId,",")),
-        n_alts = n_prot_alts + n_pref_alts,
-       conditional = ifelse(Protected+Preference == 0, 0, 1),
        y = ifelse(protectionApplied == TRUE, 1,0 )
          ) %>%
   group_by(assetId) %>%
@@ -34,24 +19,59 @@ cf = counterfactuals %>%
   ungroup()
 
 # unique at the pick level
-cf_sum = left_join(cf %>%
+traded_picks = left_join(cf %>%
   group_by(assetId,draftTeam, originTeam, idPlayer, brefUrl,
            proSportsName, draftYear, round, pick) %>%
   summarize(n_trades = n(),
          n_desc = n_distinct(desc),
          first = min(Date),
          Protected = max(Protected),
-         protectionApplied = max(y)) %>%
+         protectionApplied = max(y),
+         n_alts = max(n_prot_alts)) %>%
   ungroup(),
   draftFinal %>% select(Date, idPlayer, yearSeasonFirst, yearSeasonLast, total_gm, avg_ws, avg_bpm, avg_vorp) %>%
     mutate(Date = as.Date(Date))) %>%
   mutate(date_diff = as.numeric(Date - as.Date(first)))
 
+## for descriptive summary table
+traded_picks %>% 
+  group_by(round) %>% 
+  summarize(n_picks = n(), 
+            n_transactions = sum(n_trades), 
+            never_played = sum(is.na(yearSeasonFirst)),
+            pct_never_played =  sum(is.na(yearSeasonFirst))/ n(),
+            avg_trades = mean(n_trades),
+            sd_trades = sd(n_trades),
+            avg_dif = mean(date_diff),
+            sd_dif = sd(date_diff),
+            protected = sum(Protected),
+            pct_protected = sum(Protected)/n(),
+            prot_applied = sum(protectionApplied),
+            pct_applied = sum(protectionApplied)/sum(Protected)) %>%
+  reshape2::melt(id.vars = "round") %>%
+  spread(round, value)
+
+  allIds = rbind(cf %>% filter(!is.na(cf_ProtectionId)) %>%
+  separate_longer_delim(cf_ProtectionId, ",") %>% 
+  select(cf_ProtectionId) %>% rename(idPlayer = cf_ProtectionId),
+  cf %>% filter(!is.na(cf_PreferenceId)) %>%
+    separate_longer_delim(cf_PreferenceId, ",") %>% 
+    select(cf_PreferenceId) %>% rename(idPlayer = cf_PreferenceId),
+  cf %>% distinct(idPlayer))
+allIds = unique(allIds$idPlayer)
+
+## total N players involved as traded/potentially traded assets 
+## limited to those drafted 2005-2024
+nrow(allPlayers %>% filter(idPlayer %in% allIds))
+rm(allIds)
+
+##create table of protected transactions
 protected_assets = cf %>% 
   filter(Protected == 1) %>%
-  distinct(assetId, desc, .keep_all = T) %>%
+  distinct(assetId, desc, .keep_all = T) %>% #distinct by description/player
   mutate(doc_id = row_number())
 
+#get list of team and player names for removal from text
 teams = unique(cf$draftTeam)
 names = rbind(cf %>%
   select(proSportsName), 
@@ -65,22 +85,24 @@ names = rbind(cf %>%
   mutate(proSportsName = gsub("(", "\\(",proSportsName,fixed = TRUE),
          proSportsName = gsub(")", "\\)", proSportsName,fixed = TRUE))
 names = unique(names$proSportsName)
+
 protected_assets = protected_assets %>%
   rowwise() %>%
-  mutate(desc2 = gsub(Acquired, "ACQUIRING_TEAM", desc),
+  mutate(desc2 = gsub(Acquired, "ACQUIRING_TEAM", desc), ## text cleaning step 1
          desc2 = gsub(Relinquished, "RELINQUISHING_TEAM", desc2)) %>% 
   ungroup() %>%
-  mutate(desc2 = gsub(paste0(teams, collapse="|"), "TEAM",desc2), #mask team
-         desc2 = gsub(paste0(names, collapse="|"), "PLAYER", desc2), #mask player
+  mutate(desc2 = gsub(paste0(teams, collapse="|"), "TEAM",desc2), #step 2
+         desc2 = gsub(paste0(names, collapse="|"), "PLAYER", desc2), #step 3
          desc2 = gsub("#", "", desc2, fixed = TRUE), #remove #s
          desc2 = gsub("\\(not exercised.*?\\)", "", desc2)) %>% ##here down remove the outcome from the text
   rowwise() %>%
   mutate(
          desc2 = gsub(paste0("\\(",draftYear," ",pick,"-PLAYER\\)"), "", desc2),
-         desc2 = gsub(paste0("\\(",pick,"-PLAYER\\)"),"", desc2)
+         desc2 = gsub(paste0("\\(",pick,"-PLAYER\\)"),"", desc2) #end step 3
          ) %>%
   ungroup()
-#remove all team and player names first
+
+# create text corpus (pre-processing for text cleaning step 5 - date removal)
 df_corp = corpus(protected_assets,
                  docid_field = "doc_id",
                  text_field = "desc2")
@@ -88,9 +110,14 @@ df_corp = corpus(protected_assets,
 spacy_initialize()
 df_spacy = spacy_parse(df_corp, lemma = FALSE, tag = TRUE, entity = TRUE, 
                        nounphrase = TRUE, dependency = TRUE, pos = TRUE)  
-ent = entity_extract(df_spacy, type = "all")
+ent = entity_extract(df_spacy, type = "all") #extract entities
 spacy_finalize()
 
+years = 2003:2027
+date = ent %>% filter(entity_type == "DATE" & grepl(paste0(years, collapse="|"), entity))
+date = unique(date$entity) #identify all date entities as stop-words for removal
+
+#function for text cleaning step 4
 spacy_ent = function(x){
   tokens = spacy_parse(x) %>%
     entity_consolidate()
@@ -101,29 +128,8 @@ spacy_ent = function(x){
   res[empty] = lapply(seq_len(sum(empty)), function(x) character(0))
   res
 }
-spacy_np = function(x){
-  tokens = spacy_parse(x, nounphrase = TRUE) %>%
-    nounphrase_consolidate()
-  token_list = split(tokens$token, tokens$doc_id)
-  names(token_list) = gsub("text", "", names(token_list))
-  res = unname(token_list[as.character(seq_along(x))])
-  empty = length(res) == 0
-  res[empty] = lapply(seq_len(sum(empty)), function(x) character(0))
-  res
-}
-spacy_dep = function(x){
-  tokens = spacy_parse(x, pos = TRUE)
-  token_list = split(tokens$pos, tokens$doc_id)
-  names(token_list) = gsub("text", "", names(token_list))
-  res = unname(token_list[as.character(seq_along(x))])
-  empty = length(res) == 0
-  res[empty] = lapply(seq_len(sum(empty)), function(x) character(0))
-  res
-}
-years = 2003:2027
-date = ent %>% filter(entity_type == "DATE" & grepl(paste0(years, collapse="|"), entity))
-date = unique(date$entity)
 
+#data prep for question #2                      
 cf_data =  protected_assets %>% filter(protectionApplied & !is.na(cf_ProtectionId)) %>%
   separate_longer_delim(cf_ProtectionBref, ",") %>%
   select(tradeId, draftYear, round, pick, idPlayer,brefUrl, desc2, cf_ProtectionBref)
@@ -195,7 +201,7 @@ cf_perf = cf_perf %>%
          ws_48 = as.numeric(ws_48),
          bpm = as.numeric(bpm))
 
-save.image("~/bball.RData")
+#save.image("~/bball.RData")
 
 
 
